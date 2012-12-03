426738df9577c4b34658701dfaf222637e263180
Cody Soyland <cody@soyland.org>
2012-12-03 15:24:19 -0600

    Add more instance types.

--- shaker/__init__.py
+++ shaker/__init__.py
@@ -19,14 +19,20 @@ RUN_INSTANCE_TIMEOUT = 180  # seconds
 InstanceTypes = [
     't1.micro',
     'm1.small',
+    'm1.medium',
     'm1.large',
+    'm1.xlarge',
     'm2.xlarge',
     'm2.2xlarge',
     'm2.4xlarge',
+    'm3.xlarge',
+    'm3.2xlarge',
     'c1.medium',
     'c1.xlarge',
     'cc1.4xlarge',
     'cc2.8xlarge',
+    'cg1.4xlarge',
+    'hi1.4xlarge',
     ]
 
 
